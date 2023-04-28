65fdfbfe530682a9413918fa68f131d75ce4fd98
sk199 <sk1997@gmx.net>
2023-04-28 14:23:03 +0200

    Exercise 1

---
+++ main.py
@@ -0,0 +1,71 @@
+# # 1: famous quote
+#
+# quote = '"Even I don´t wake up looking like Cindy Crawford"'
+# author = "Cindy Crawford"
+# print(author, "once said", quote)
+# #Result: Cindy Crawford once said "Even I don´t wake up looking like Cindy Crawford"
+
+# 2: number eight
+
+# def number_eight()
+#     print(5+3)
+#     print(5+3)
+#     print(5+3)
+#     print(5+3)
+#     print(5+3)
+
+# 3: formatting
+
+#Lists
+l = []
+
+# # Adding Element into list
+# l.append(5)
+# l.append(10)
+# print("Adding 5 and 10 in list", l)
+#
+# # Popping Elements from list
+# l.pop()
+# print("Popped one element from list", l)
+# print()
+#
+# Set
+s = set()
+
+# Adding element into set
+s.add(5)
+s.add(10)
+print("Adding 5 and 10 in set", "Länge", len(s))
+
+print(s.discard(5))
+
+
+# # Removing element from set
+# s.remove(5)
+# print("Removing 5 from set", s)
+# print()
+#
+# # Tuple
+# t = tuple(l)
+#
+# # Tuples are immutable
+# print("Tuple", t)
+# print()
+
+# Dictionary
+d = {}
+
+# # Adding the key value pair
+# d[5] = "Five"
+# d[10] = "Ten"
+# print("Dictionary", len(d))
+#
+# print(d[5])
+#
+# # Removing key-value pair
+# del d[10]
+# print("Dictionary", d)
+
+
+
+
