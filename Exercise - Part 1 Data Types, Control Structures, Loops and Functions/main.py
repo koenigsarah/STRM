8a8dd4287a65e0ad09606a4afa5d73e53ad66b65
sk199 <sk1997@gmx.net>
2023-04-28 14:37:58 +0200

    Exercise 1

---
+++ Exercise - Part 1 Data Types, Control Structures, Loops and Functions/main.py
@@ -0,0 +1,17 @@
+#1: famous quote
+
+quote = '"Even I don´t wake up looking like Cindy Crawford"'
+author = "Cindy Crawford"
+print(author, "once said", quote)
+#Result: Cindy Crawford once said "Even I don´t wake up looking like Cindy Crawford"
+
+#2: number eight
+#
+# def number_eight()
+#     print(5+3)
+#     print(5+3)
+#     print(5+3)
+#     print(5+3)
+#     print(5+3)
+
+#3: formatting
