6d9e619c0bf40fccc8d21adde34a05e089ec4654
koenigsarah <sk1997@gmx.net>
2023-05-25 17:04:42 +0200

    Exercise 1

--- Exersice - Part 3/König Sarah Exercise - Part 3.py
+++ Exersice - Part 3/König Sarah Exercise - Part 3.py
@@ -1,4 +1,4 @@
-#Task 1: Some Classes and String represention
+# Task 1: Some Classes and String represention
 """You are responsible for the design and implementation of a ticket system. Customers create tickets
 with issues that they face when working with a software system.
 A Ticket has
@@ -23,80 +23,113 @@ available, the error code (str). Ids of hardware tickets start at 200.000.
 Design the classes necessary to manage these tickets, write constructors for these classes. Make sure
 that the classes can represent themselves as strings."""
 
+from enum import Enum, IntEnum  # wie am 15.05 besprochen, Enum und Intenum
+from datetime import datetime  # für Timestamp
+from time import sleep # zurzeit auskommentiert
 
-from enum import Enum, IntEnum #wie am 15.05 besprochen, Enum und Intenum
-from datetime import datetime #für Timestamp
+print("Task 1: ", "\n")
 
 class Ticket:
-    class Priority(IntEnum):#Unterklasse von Enum, für Ganzzahlen (als int behandelt)
+    class Priority(IntEnum):  # Unterklasse von Enum, für Ganzzahlen (als int behandelt)
         SEVERE, SIGNIFICANT, AFFECTING, NON_CRITICAL, ROUTINE = 1, 2, 3, 4, 5
 
-    class Status(Enum):#Unterklasse, begrenzte Anzahl von Optionen darstellen (als string behandelt))
+    class Status(Enum):  # Unterklasse, begrenzte Anzahl von Optionen darstellen (als string behandelt))
         CREATED, IN_PROGRESS, TESTING, CLOSED = "Created", "In progress", "Testing", "Closed"
 
-    def __init__(self, description, priority, status=Status.CREATED, created_timestamp=None):#für Individualisierung, erster Parameter muss self sein, Attribute des Tickets hier; created_timestamp=None, weil sonst Syntax Error (Non-default argument follows default argument)
-        self.id = None
+    def __init__(self, description, priority, status=Status.CREATED,
+                 created_timestamp=None):  # für Individualisierung, erster Parameter muss self sein, Attribute des Tickets hier; created_timestamp=None, weil sonst Syntax Error (Non-default argument follows default argument) - automatische Formatierung formatiert es mir so
+        self.id = None  # damit die ID erst bei Hard/Software hinzugefügt wird, da sie spezifisch ist (startet mit unterschiedlichen Nummern)
         self.description = description
         self.priority = priority
         self.status = status
-        self.created_timestamp = created_timestamp if created_timestamp else datetime.now() #wenn Datum da ist, dann das nehmen - sont datetime.now() erstellen
-        self.comments = []#für Liste
+        self.created_timestamp = created_timestamp if created_timestamp else datetime.now()  # wenn Datum da ist, dann das nehmen - sonst datetime.now() erstellen
+        self.comments = []  # für Liste (a list of comments)
 
-    def add_comment(self, text, user):#Methode für mit der ein Kommentar zum Ticket hinzugefügt werden kann mit Text, User und Timestamp dabei
-        timestamp = datetime.now() #Timestamp von der Jetzt-Zeit, wenn Kommentar hinzugefügt wird
-        self.comments.append((text, user, timestamp))#zum Kommentar text, user und timestamp anhängen(self.comments damit man auf comments von def__init__ zugreifen kann zugreifen kann)
+    def add_comment(self, text,
+                    user):  # Methode mit der ein Kommentar zum Ticket hinzugefügt werden kann mit Text, User und Timestamp dabei
+        timestamp = datetime.now()  # Timestamp von der Jetzt-Zeit, wenn Kommentar hinzugefügt wird
+        self.comments.append((text, user,
+                              timestamp))  # zum Kommentar text, user und timestamp anhängen(self.comments damit man auf comments von def__init__ oberhalb zugreifen kann)
 
-    def __str__(self):#Methode um Ticket-Objekt zu String zu machen
-        priority_str = f"Priority {self.priority.value} ({self.priority.name.replace('_', ' ')})" #Priority als Strg
-        created_str = f"Created on {self.created_timestamp.strftime('%m/%d/%y %H:%M:%S')}" #Zeitstempel als str und in folgendem Datumformat (wie in Video von Studyflix erklärt)
-        comment_str = "\nComments:" + "\n".join(f" {user}, {timestamp.strftime('%m/%d/%y %H:%M:%S')}: {text}" for text, user, timestamp in self.comments) #Kommentar - von wem, mit wann +Text
+    def __str__(self):  # Methode um Ticket-Objekt zu String zu machen
+        priority_str = f"Priority {self.priority.value} ({self.priority.name.replace('_', ' ')})"  # Priority als Strg (ist aber schon wegen ENUM string), ich will von Priority den Wert, dann in Klammern das Keyword - self damit ich auf das von oben zugreifen kann
+        created_str = f"Created on {self.created_timestamp.strftime('%m/%d/%y %H:%M:%S')}"  # Zeitstempel als str und in folgendem Datumformat (wie in Video von Studyflix erklärt), self. damit ich auf created_timestamp von oben zugreifen kann
+        comment_str = "\nComments:" + "\n".join(
+            f" {user}, {timestamp.strftime('%m/%d/%y %H:%M:%S')}: {text}" for text, user, timestamp in
+            self.comments)  # Kommentar - von wem, mit wann in welcher Formatierung +Text für text, user, timestamp in self.comments (dort hab ich vorher mit def add_commment - self.comment.append die Komm. drangehöngt)
 
-        return f"T: {self.id}: {self.description}, {created_str} - {priority_str} {comment_str}" #Und jetzt die oberen drei zusammen returnen und mit f String, für die Platzhalter, allgemeine Infos - das andere ist dann Software / Hardware spezifisch
+        return f"T: {self.id:_}: {self.description}, {created_str} - {priority_str} {comment_str}"  # Und jetzt die oberen drei zusammen returnen und mit f String, für die Platzhalter, allgemeine Infos - das andere ist dann Software / Hardware spezifisch
 
-class SoftwareTicket(Ticket):#eigene Klasse, ergbt von "Ticket" Klasse, zusätzliche Attribute wie Fehlermeldung und betroffene Betriebssysteme
-    software_ticket_id = 100000 #Start bei angegebener Nummer
+# Funktioniert oben mal alles - allgemeine Zeile passt
 
-    class OperatingSystem(Enum):#Unterklasse, begrenzte Anzahl von Optionen darstellen (als string behandelt)
+
+class SoftwareTicket(Ticket):  # eigene Klasse, erbt von "Ticket" Klasse, zusätzliche Attribute: S-Ticket ID, Fehlermeldung und betroffene Betriebssysteme
+    software_ticket_id = 100_000  # Start bei angegebener Nummer
+
+    class OperatingSystem(Enum):  # Unterklasse, begrenzte Anzahl von Optionen darstellen (als string behandelt)
         WINDOWS, MACOS, LINUX, ANDROID, IOS = "Windows", "macOS", "Linux", "Android", "iOS"
 
-    def __init__(self, description, priority, operating_systems, error_message, status=Ticket.Status.CREATED, created_timestamp=None):#Attribute festlegen, wieder bei init zuerst self vorne
-        super().__init__(description, priority, status, created_timestamp)#damit ich Methode der Klasse "Tickets" aufrufen kann (dort festgelegte attribute)
-        self.id = SoftwareTicket.software_ticket_id
+    def __init__(self, description, priority, operating_systems, error_message, status=Ticket.Status.CREATED,
+                 created_timestamp=None):  # Attribute festlegen, wieder bei init zuerst self vorne
+        super().__init__(description, priority, status,
+                         created_timestamp)  # damit ich Methode der Klasse "Tickets" aufrufen kann (dort festgelegte attribute)
+        self.id = SoftwareTicket.software_ticket_id #ich greife auf software_ticket_id von oben zu, um es um 1 zu erhöhen (bei neuem Ticket) - damit wird die id von class Ticket überschreiben (vorher ID = None)
         SoftwareTicket.software_ticket_id += 1
         self.operating_systems = operating_systems
+        # print(self.operating_systems)
+        # print(self.operating_systems.value)
         self.error_message = error_message
 
-    def __str__(self):#Methode um Ticket-Objekt als String darzustellen
-        base_str = super().__str__() #damit rufe ich die Eltern-Klasse Ticket (den return f)
-        os_str = self.operating_systems #man kann nur an iterable joinen, noch korrigieren
-        return f"{base_str}\n Error message: '{self.error_message}', Affected OSs: {os_str}"
+    def __str__(self):  # Methode um Ticket-Objekt als String darzustellen, OperatingSystems durch Enum schon string, nicht mehr notwendig
+        base_str = super().__str__()  # damit rufe ich die Eltern-Klasse Ticket (den return f)
+        return f"{base_str}\n Error message: '{self.error_message}', Affected OSs: {self.operating_systems.value}"
 
-class HardwareTicket(Ticket): #eigene Klasse, erbet von Klasse "Ticket"
-    hardware_ticket_id = 200000 #Start bei angegebener Nummer
+    def __repr__(self):  # Methode um Ticket-Objekt als String darzustellen, OperatingSystems durch Enum schon string, nicht mehr notwendig
+        base_str = super().__str__()  # damit rufe ich die Eltern-Klasse Ticket (den return f)
+        return f"{base_str}\n Error message: '{self.error_message}', Affected OSs: {self.operating_systems.value}"
 
-    def __init__(self, description, priority, component, serial_number, error_code=None, status=Ticket.Status.CREATED, created_timestamp=None):
-        super().__init__(description, priority, status, created_timestamp)#damit ich Methode der Klasse "Tickets" aufrufen kann
+
+class HardwareTicket(Ticket):  # eigene Klasse, erbt von Klasse "Ticket"
+    hardware_ticket_id = 200_000  # Start bei angegebener Nummer
+
+    def __init__(self, description, priority, component, serial_number, error_code=None, status=Ticket.Status.CREATED,
+                 created_timestamp=None):
+        super().__init__(description, priority, status,
+                         created_timestamp)  # damit ich Methode der Klasse "Tickets" aufrufen kann
         self.id = HardwareTicket.hardware_ticket_id
         HardwareTicket.hardware_ticket_id += 1
         self.component = component
         self.serial_number = serial_number
         self.error_code = error_code
 
-    def __str__(self):#Methode um Ticket-Objekt als String darzustellen
+    # def __str__(self):  # Methode um Ticket-Objekt als String darzustellen
+    #     base_str = super().__str__()
+    #     component_str = f"Component: {self.component}, Seriennummer: {self.serial_number}, Error code: {self.error_code or 'None'}"
+    #     return f"{base_str}\n {component_str}"
+
+    def __repr__(self):  # Methode um Ticket-Objekt als String darzustellen
         base_str = super().__str__()
-        component_str = f"Component: {self.component}, s/n: {self.serial_number}, error code: {self.error_code or 'None'}"
+        component_str = f"Component: {self.component}, Seriennummer: {self.serial_number}, Error code: {self.error_code or 'None'}"
         return f"{base_str}\n {component_str}"
 
-# Beispiel:
-software_ticket = SoftwareTicket("Login problems", Ticket.Priority.SEVERE, SoftwareTicket.OperatingSystem.WINDOWS, "Document not found: 404")
-software_ticket.add_comment("Android does not have that problem", "Tina")
-software_ticket.add_comment("Android is developed separately", "Christoph")
 
-hardware_ticket = HardwareTicket("Troubles with printer in main hall", Ticket.Priority.AFFECTING, "Printer C.1.08b", "234-23")
+# Beispiel:
+software_ticket = SoftwareTicket("Login problems", Ticket.Priority.SEVERE, SoftwareTicket.OperatingSystem.WINDOWS,
+                                 "Document not found: 404")
+software_ticket.add_comment("Android does not have that problem", "Tina")  # 1.Kommentar
+software_ticket.add_comment("Android is developed separately", "Christoph")  # 2.Kommentar
+
+
+# sleep(5)
+hardware_ticket = HardwareTicket("Troubles with printer in main hall", Ticket.Priority.AFFECTING, "Printer C.1.08b",
+                                 "234-23")  # Nach und nach die fStrings auffüllen (zuerst allg. Ticket, dann Hardware Ticket)
+
+software_ticket2 = SoftwareTicket("Layout issues", Ticket.Priority.AFFECTING, SoftwareTicket.OperatingSystem.LINUX, "ValueError")
+
 
 print(software_ticket, "\n")
+print(software_ticket2, "\n")
 print(hardware_ticket, "\n")
-
 
 ##Task 2
 """Internally, the Ticket System assigns tickets to various teams. Each team has a list of assigned tickets.
@@ -108,31 +141,42 @@ of Tickets assigned to a particular team. It has two methods:
 1. add(team: Team, ticket: Ticket) to add a ticket and 
 2. a string representation.
 """
+print("\n", "Task 2")
 
 class Team:
-    def __init__(self, name, *members): #für Individualisierung, * vor Members, um variable Anzahl von Argumenten für "members" aufzunehmen
+    def __init__(self, name,
+                 *members):  # für Individualisierung, * vor Members, um variable Anzahl von Argumenten für "members" aufzunehmen
         self.name = name
-        self.members = list(members)#um Mitglieder in Liste abzuspeichern
+        self.members = list(members)  # um Mitglieder in Liste abzuspeichern
 
-    def __str__(self): #wieder als String darstellen
-        return f"Team {self.name} ({', '.join(self.members)}):"#join um hier {} wegzugeben und Beistrich zwischen den Namen
+    def __str__(self):  # wieder als String darstellen, was oben in __init__ definiert wurde
+        return f"Team {self.name} ({', '.join(self.members)}):"  # join um hier {} wegzugeben und Beistrich zwischen den Namen
 
-class Assignments:
+
+class Assignments:  # Ticketzuweisungen
     def __init__(self):
-        self.assignments = {}#Dictonary
+        self.assignments = {}  # Dictonary
 
-    def add(self, team, ticket):
+    def add(self, team, ticket):  # Methode add, um Team Ticket zuzuweisen
         if team not in self.assignments:
             self.assignments[team] = []
-        self.assignments[team].append(ticket)
+        self.assignments[team].append(ticket)  # 
 
-    def get(self, team):
+    def get(self, team):  # Methode get, um Zuweisung von Team abzurufen
         if team in self.assignments:
             return self.assignments[team]
         else:
-            return []
+            return []  # wenn Team in assignment ist, dann assignemts[team9 returnen, sonst []
 
-    def __str__(self):
+    # def __str__(self):  # wieder zu str
+    #     result = "All assignments:\n"
+    #     for team, tickets in self.assignments.items():  # für Team und Tickets in self assignment, result += str(team und Absatz)
+    #         result += str(team) + "\n"
+    #         for ticket in tickets:  # für ticket in Tickets, result += ticket und Abstand (vorher war ja Team dazu bei result)
+    #             result += str(ticket) + "\n"
+    #     return result
+
+    def __repr__(self):  # Empfohlen von Alex: für textuelle Repräsentation des Objekts (jetzt ist auch der Fehler weg - hab vorher neue Tickets erstellt im 2. Part (ungewollt)
         result = "All assignments:\n"
         for team, tickets in self.assignments.items():
             result += str(team) + "\n"
@@ -140,21 +184,19 @@ class Assignments:
                 result += str(ticket) + "\n"
         return result
 
+
 # Beispiel
 assignments = Assignments()
 t1, t2 = Team("1", "Tina", "Philip"), Team("2", "Theresa", "Mirela")
 
-assignments.add(t1, HardwareTicket("Troubles with printer in main hall", Ticket.Priority.AFFECTING,
-                                  "Printer C.1.08b", "234-23"))
-
-assignments.add(t2, SoftwareTicket("Login problems", Ticket.Priority.SEVERE,
-                                   "Document not found: 404", "MacOS", "Windows"))
-
-assignments.add(t2, SoftwareTicket("Layout issues", Ticket.Priority.AFFECTING,
-                                   "ValueError", "Windows"))
+assignments.add(t1, hardware_ticket)
+assignments.add(t2, software_ticket)
+assignments.add(t2, software_ticket2)
 
 print(assignments)
 
-#Nur Aufgaben,die zu einem Team zugeordnet sind
+# Nur Aufgaben, die zu einem Team zugeordnet sind
 print("Assignments of " + str(t1))
 print(assignments.get(t1))
+
+print(assignments.assignments[t2])
